3be172197d37eb5e8ba34366b58d661d69d91a8e
Goncalo Leiria <goncalo.faria1@gmail.com>
2022-08-03 20:42:56 +0100

    finished initial CRUD UI for users app

--- users/models.py
+++ users/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from numpy import integer
 
 # Create your models here.
 
@@ -6,3 +7,7 @@ from django.db import models
 class Users(models.Model):
     firstname = models.CharField(max_length=255)
     lastname = models.CharField(max_length=255)
+    age = models.IntegerField(blank=True, null=True)
+    country = models.CharField(max_length=200, blank=True, null=True)
+    email_field = models.EmailField(
+        max_length=255, unique=True, blank=True, null=True)
--- users/views.py
+++ users/views.py
@@ -1,16 +1,62 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
+from django.urls import reverse
 from .models import Users
 import pandas as pd
-# Create your views here.
 
 
 def index(request):
     myusers = Users.objects.all().values()
-    df = pd.DataFrame(myusers)
     template = loader.get_template("index.html")
     context = {
-        "myusers": df.to_html()
+        "myusers": myusers
     }
-    print(df)
     return HttpResponse(template.render(context, request))
+
+
+def add(request):
+    template = loader.get_template("add.html")
+    return HttpResponse(template.render({}, request))
+
+
+def addrecord(request):
+    f = request.POST["first"]
+    l = request.POST["last"]
+    e = request.POST["email"]
+    a = request.POST["age"]
+    c = request.POST["country"]
+    user = Users(firstname=f, lastname=l, email_field=e, age=a, country=c)
+    user.save()
+    return HttpResponseRedirect(reverse("index"))
+
+
+def delete(request, id):
+    user = Users.objects.get(id=id)
+    user.delete()
+    return HttpResponseRedirect(reverse("index"))
+
+
+def update(request, id):
+    myuser = Users.objects.get(id=id)
+    template = loader.get_template("update.html")
+    context = {
+        "myuser": myuser,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def updaterecord(request, id):
+    f = request.POST["first"]
+    l = request.POST["last"]
+    e = request.POST["email"]
+    a = request.POST["age"]
+    c = request.POST["country"]
+    user = Users.objects.get(id=id)
+    user.firstname = f
+    user.lastname = l
+    user.email_field = e
+    user.age = a
+    user.country = c
+    user.save()
+
+    return HttpResponseRedirect(reverse("index"))
