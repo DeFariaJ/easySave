e7e5740e9b6263fd3e80278dd426af9f8ef9e886
Goncalo Leiria <goncalo.faria1@gmail.com>
2022-08-05 11:00:19 +0100

    reduced bills model for testing. Will play with DateField user input. Want a snapshot here

--- bills/models.py
+++ bills/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from datetime import datetime, date
 # Create your models here.
+
+
+class Bills(models.Model):
+    fixed_bills = models.CharField(max_length=255)
+    fixed_bills_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    pay_date = models.DateField(auto_now_add=False, auto_now=False, blank=True)
+    timestamp = models.DateField(auto_now_add=True, auto_now=False, blank=True)
