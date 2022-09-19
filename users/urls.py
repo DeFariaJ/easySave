92102b56b82fe2458a655ce6799b3eb75977006b
Goncalo Leiria <goncalo.faria1@gmail.com>
2022-09-19 19:30:11 +0100

    userCreationForm to sign users up from frontend

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 
 urlpatterns = [
-
+    path("signup/", views.signup_view, name="signup"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,23 @@
 from re import template
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
 import pandas as pd
+from django.contrib.auth.forms import UserCreationForm
+
+
+def signup_view(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+        # later we'll log user in here
+        return redirect("index_bills")
+    else:
+
+        form = UserCreationForm()
+        context = {
+            "form": form,
+        }
+    return render(request, "signup.html", context)
