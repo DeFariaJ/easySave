54fc623f5c9cd61736b66e210ff162d51ed745d9
Goncalo Leiria <goncalo.faria1@gmail.com>
2022-09-20 08:43:11 +0100

    login form working. logging user in not yet.

--- users/views.py
+++ users/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
 import pandas as pd
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 
 def signup_view(request):
@@ -13,7 +13,7 @@ def signup_view(request):
         if form.is_valid():
             form.save()
         # later we'll log user in here
-        return redirect("index_bills")
+        return redirect("total")
     else:
 
         form = UserCreationForm()
@@ -21,3 +21,18 @@ def signup_view(request):
             "form": form,
         }
     return render(request, "signup.html", context)
+
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            # login uuser
+            return redirect("total")
+
+    else:
+        form = AuthenticationForm()
+    context = {
+        "form": form,
+    }
+    return render(request, "login.html", context)
